d6976cc9d827749e515e2bc14775c97a5f9f84cc
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-25 23:05:31 +0530

    Top 50 according to the year specified by the user.

--- IMDB_Top50_Scraper.py
+++ IMDB_Top50_Scraper.py
@@ -9,25 +9,23 @@ opener.addheaders = [('User-agent','Mozilla/5.0')]
 url = "http://www.imdb.com/search/title?release_date=" + year + "," + year + "&title_type=feature"
 ourUrl = opener.open(url).read()
 soup = BeautifulSoup(ourUrl)
-
-article = soup.find('div', attrs={'class':'article'}).find('h1')
+article = soup.find('div', attrs={'class': 'article'}).find('h1')
 print article.contents[0] + ': '
 
-lister_list_contents = soup.find('div', attrs={'class':'lister-list'})
-
+lister_list_contents = soup.find('div', attrs={'class': 'lister-list'})
 i = 1
-itermovie = soup.findAll('div', attrs={'class':'lister-item mode-advanced'})
+itermovie = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
 for div in tqdm(itermovie):
     print str(i) + '.',
-    header = div.findChildren('h3', attrs={'class':'lister-item-header'})
+    header = div.findChildren('h3', attrs={'class': 'lister-item-header'})
     for item in header:
         title = header[0].findChildren('a')
         print 'Movie: ' + str(title[0].contents[0])
 
-    genre = div.findChildren('span', attrs={'class':'genre'})
+    genre = div.findChildren('span', attrs={'class': 'genre'})
     genre_contents = genre[0].text.encode('utf-8').decode('ascii', 'ignore')
     print 'Genre: ' + genre_contents
-    description = div.findChildren('p',attrs={'class':'text-muted'})
+    description = div.findChildren('p', attrs={'class': 'text-muted'})
     description_text = description[0].text.encode('utf-8').decode('ascii', 'ignore')
     print 'Description: ' + description_text
     i += 1
