6e16d28ac1cc26bdaf68613007922585aa59cb33
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-27 07:46:27 +0530

    Movie name

--- 2019_Update/scraper.py
+++ 2019_Update/scraper.py
@@ -1,16 +1,19 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import datetime
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 def main(): # Main function
 	current_year = int(datetime.datetime.now().year)
 	url = "http://www.imdb.com/search/title?release_date=" + str(current_year) + "," + str(current_year) + "&title_type=feature"
 	html = urlopen(url)
 	soup = BeautifulSoup(html.read(), features="html.parser")
-	dataset_top50 = []
+	dataset_top50 = {}
+	id = 1
 	movies_list = soup.findAll('div', attrs={'class': 'lister-item-content'})
 	for each in movies_list:
-		print(each)
+
 		# Prototype of each movie item
 		movie_item = {
 			'name': '',
@@ -24,6 +27,14 @@ def main(): # Main function
 			'gross': ''
 		}
 
+		if each.find('h3', attrs={'class': 'lister-item-header'}).find('a').text:
+			name_value = each.find('h3', attrs={'class': 'lister-item-header'}).find('a').text
+			movie_item['name'] = name_value
+
+		dataset_top50[id] = movie_item
+		id += 1
+
+	pp.pprint(dataset_top50)
 
 	# End of the main function
 
