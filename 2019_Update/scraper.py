a401e1e562e44280a5ecd6805aaf319af9484951
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-27 07:36:46 +0530

    Update

--- 2019_Update/scraper.py
+++ 2019_Update/scraper.py
@@ -7,9 +7,23 @@ def main(): # Main function
 	url = "http://www.imdb.com/search/title?release_date=" + str(current_year) + "," + str(current_year) + "&title_type=feature"
 	html = urlopen(url)
 	soup = BeautifulSoup(html.read(), features="html.parser")
-	dataset_top50 = {}
-	movies_list = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
-	print(movies_list)
+	dataset_top50 = []
+	movies_list = soup.findAll('div', attrs={'class': 'lister-item-content'})
+	for each in movies_list:
+		print(each)
+		# Prototype of each movie item
+		movie_item = {
+			'name': '',
+			'certificate': '',
+			'runtime': '',
+			'genre': '',
+			'description': '',
+			'director': '',
+			'stars': [],
+			'votes': '',
+			'gross': ''
+		}
+
 
 	# End of the main function
 
