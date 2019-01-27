91d2a30e1425564e5b8f0ac1030ee65f8d0aa8f4
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-27 07:27:57 +0530

    Updatet

--- 2019_Update/scraper.py
+++ 2019_Update/scraper.py
@@ -7,7 +7,11 @@ def main(): # Main function
 	url = "http://www.imdb.com/search/title?release_date=" + str(current_year) + "," + str(current_year) + "&title_type=feature"
 	html = urlopen(url)
 	soup = BeautifulSoup(html.read(), features="html.parser")
-	print(soup)
+	dataset_top50 = {}
+	movies_list = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
+	print(movies_list)
+
+	# End of the main function
 
 
 main() # Call of the main function
