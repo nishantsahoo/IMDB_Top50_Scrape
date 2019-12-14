70c8d4e2f237952e6398180e83116bf6f01eb93e
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-12-14 16:33:16 +0530

    Update

--- IMDB_Top50_Scraper.py
+++ IMDB_Top50_Scraper.py
@@ -5,15 +5,15 @@ import datetime
 import requests
 import os
 
-dataset_location = os.path.realpath(os.path.join( os.path.dirname(__file__),"DataSets"))
+dataset_location = os.path.realpath(os.path.join(os.path.dirname(__file__), "DataSets"))
 current_year = int(datetime.datetime.now().year)
 headers= {'User-agent': 'Mozilla/5.0'}
-input_year = int(input("Please enter start year.such as 2016:\n"))
+input_year = int(input("Please enter start year (eg. 2016): "))
 if input_year > current_year:
-    print("No movie is recorded in year %i yet!!"%(input_year))
+    print("No movie is recorded in year %i yet!!" % (input_year))
 else:
     for year in tqdm(range(input_year, current_year+1)):
-        sys.stdout = open(os.path.join(dataset_location,"IMDB_Top50_"+str(year) + '.txt') , 'w')
+        sys.stdout = open(os.path.join(dataset_location,"IMDB_Top50_" + str(year) + '.txt'), 'w+')
         url = "http://www.imdb.com/search/title?release_date=" + str(year) + "," + str(year) + "&title_type=feature"
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
